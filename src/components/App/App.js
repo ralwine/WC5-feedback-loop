95a7d0dacd9c40cb6d81aec5f63dd520f5ea08f9
Ryan M Alwine <130316550+ralwine@users.noreply.github.com>
2023-08-17 19:00:59 -0500

    Initialize hooks

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Feedback!</h1>
+        <h4><i>Don't forget it!</i></h4>
+      </header>
+      <br />
+    </div>
+  );
+
+}
+
+export default App;
