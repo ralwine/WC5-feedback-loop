c5444a1dc9ecdcb5e403b672cf5acc7737b8c733
Ryan M Alwine <ryanmitchellalwine@gmail.com>
2023-08-19 15:48:48 -0500

    added click handling in FeelingItem, showing in console

--- src/components/FeelingItem/FeelingItem.jsx
+++ src/components/FeelingItem/FeelingItem.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import React from 'react';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
 export function FeelingItem() {
+
+  const dispatch = useDispatch()
+  const [feelingRating, setFeelingRating] = useState('')
+
+  // HandleSubmit here
+  const handleFeelingRating = () => {
+    console.log("inside handleFeel")
+    let feelingData = {
+      feeling_rating: feelingRating
+      //not sure about this 
+    }
+    console.log("Feeling? rating", feelingData)
+    dispatch({
+      type: 'RATE_EMOTION',
+      payload: feelingData
+    })
+  }
+
   return (<>
     <header className='App-header'>
       <h1 className='App-title'>Feedback!</h1>
@@ -14,10 +34,11 @@ export function FeelingItem() {
       Feeling?
     </h2>
     {/* input id should be different for each? */}
-    <input type="number" id="inputFeeling" placeholder='Scale of 1-10' />
+    <input type="number" id="inputFeeling" value={feelingRating} placeholder='Scale of 1-10'
+      onChange={event => setFeelingRating(event.target.value)} />
     <div className='fancyDiv'>
       <Link to='/understanding'>
-        <button className="nextBtn">NEXT</button>
+        <button className="nextBtn" onClick={handleFeelingRating}>NEXT</button>
       </Link>
     </div>
   </>)
